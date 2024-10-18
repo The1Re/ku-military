2b2aa833109c7a21ac7f66d2ffe6a9dc70059afc
The1Re <kanesh132@gmail.com>
2024-10-19 03:17:56 +0700

    update not found page

--- views/page/notfound.php
+++ views/page/notfound.php
@@ -1,9 +1,14 @@
-<main class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-    <div class="text-center">
-        <p class="text-base font-semibold text-indigo-600">404</p>
-        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-        <p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
-        <div class="mt-10 flex items-center justify-center gap-x-6">
+<div class="flex justify-center items-center h-screen overflow-x-hidden">
+    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div class="text-center">
+            <p class="text-base font-semibold text-indigo-600">404</p>
+            <h1 class="text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                Page not found
+            </h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600">
+                Sorry, we couldn’t find the page you’re looking for.
+            </p>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
             <a 
                 href="?controller=page&action=home"
                 class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -11,5 +16,6 @@
                 Go back home
             </a>
         </div>
+        </div>
     </div>
-</main>
+</div>
