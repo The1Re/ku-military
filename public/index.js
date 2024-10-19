6d4ad1aab06875955d85c4c58ce308a87f66664e
The1Re <kanesh132@gmail.com>
2024-10-19 14:33:12 +0700

    fix angle in side bar

--- public/index.js
+++ public/index.js
@@ -22,24 +22,22 @@ for (let i=0; i<toggleSidebars.length; i++)
 const toggle_lists = document.getElementsByClassName('toggle-list');
 for (let i=0; i<toggle_lists.length; i++)
 {
-    const toggle_list = toggle_lists.item(i);
+    const toggle_list = toggle_lists[i];
 
     toggle_list.addEventListener('click', () => {
-        const icon = toggle_list.getElementsByTagName('i')[0];
+        const icon = toggle_list.getElementsByClassName('toggle-icon')[0];
         const dropdown = document.getElementById('dropdown');
-        dropdown.classList.toggle("hidden");
-        if (icon.classList.contains('toggle-icon')) {
-            if (icon.classList.contains('fa-angle-down')) {
-                // show
-                icon.classList.remove('fa-angle-down');
-                icon.classList.add('fa-angle-up');
-            } else {
-                // close
-                icon.classList.remove('fa-angle-up');
-                icon.classList.add('fa-angle-down');
-            }
-        }
 
+        dropdown.classList.toggle("hidden");
+        if (icon.classList.contains('fa-angle-down')) {
+            // show
+            icon.classList.remove('fa-angle-down');
+            icon.classList.add('fa-angle-up');
+        } else {
+            // close
+            icon.classList.remove('fa-angle-up');
+            icon.classList.add('fa-angle-down');
+        }
     });
 
     
