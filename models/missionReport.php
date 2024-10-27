8a3cd6aed532f559db1de8bed48dd84ee8e31359
The1Re <kanesh132@gmail.com>
2024-10-27 13:26:06 +0700

    add method missionReport model

--- models/missionReport.php
+++ models/missionReport.php
@@ -28,6 +28,51 @@ class MissionReport
         return MissionReport::db_to_object($result);
     }
 
+    public static function getById($id)
+    {
+        $sql = "
+            SELECT * FROM missionreport
+            WHERE missionReportId = ?
+        ";
+        $params = [$id];
+        $result = Database::query($sql, $params);
+        return MissionReport::db_to_object($result);
+    }
+    
+    public static function add($missionId, $detail, $date)
+    {
+        $sql = "
+            INSERT INTO missionreport
+            VALUES (?, ?, ?)
+        ";
+        $params = [$missionId, $detail, $date];
+        $result = Database::query($sql, $params);
+        return MissionReport::db_to_object($result);
+    }
+    
+    public static function update($id, $missionId, $detail, $date)
+    {
+        $sql = "
+            UPDATE missionreport
+            SET missionId = ?, detail = ?, date = ?
+            WHERE missionReportId = ?
+        ";
+        $params = [$missionId, $detail, $date, $id];
+        $result = Database::query($sql, $params);
+        return MissionReport::db_to_object($result);
+    }
+
+    public static function delete($id)
+    {
+        $sql = "
+            DELETE FROM missionreport WHERE missionReportId = ?
+        ";
+        $params = [$id];
+        $result = Database::query($sql, $params);
+        return MissionReport::db_to_object($result);
+    }
+
+
     public static function db_to_object($result_query)
     {
         $data = [];
