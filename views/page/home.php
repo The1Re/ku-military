d4a1f7d2502b44584b4c636ae41678cd9f1ff7ee
The1Re <kanesh132@gmail.com>
2024-11-04 17:21:50 +0700

    edit home page

--- views/page/home.php
+++ views/page/home.php
@@ -1,24 +1,28 @@
 <?php
     $list_user = [
         [
-            'link' => 'http://158.108.207.4/db24/db24_141/ku-military?controller=mission&action=index',
-            'id' => 6520503258,
-            'name' => 'นายคเณศ อรชุนเลิศไมตรี',
-            'detail' => 'ภารกิจ, รายงานประจำวัน, รายงานสิ้นสุดภารกิจ และ การคืนยุทโธปกรณ์'
-        ],
-        [
+            'title' => 'ส่วนที่ 1',
             'link' => 'http://158.108.207.4/db24/db24_139/',
             'id' => 6520501042,
             'name' => 'นายณัฐกิตต์ จันทพรม',
             'detail' => 'ทีมที่ปฏิบัติภารกิจ, สมาชิกทีม, รายชื่อทหาร'
         ],
         [
+            'title' => 'ส่วนที่ 2',
             'link' => 'http://158.108.207.4/db24/db24_140/',
             'id' => 6520501093,
             'name' => 'นายปกรณ์สัณห์ สุเชาว์อินทร์',
             'detail' => 'ยุทโธปกรณ์, ใบเบิกยุทโธปกรณ์, รายการยุทโธปกรณ์ที่เบิก'
         ],
         [
+            'title' => 'ส่วนที่ 3',
+            'link' => 'http://158.108.207.4/db24/db24_141/ku-military?controller=mission&action=index',
+            'id' => 6520503258,
+            'name' => 'นายคเณศ อรชุนเลิศไมตรี',
+            'detail' => 'ภารกิจ, รายงานประจำวัน, รายงานสิ้นสุดภารกิจ และ การคืนยุทโธปกรณ์'
+        ],
+        [
+            'title' => 'ส่วนที่ 4',
             'link' => 'http://158.108.207.4/db24/db24_142/',
             'id' => 6520503236,
             'name' => 'นายยุทธการ ศรีศักดา',
@@ -40,12 +44,13 @@
                     href="<?php echo $user['link']; ?>" 
                     class="block bg-blue-300 shadow-lg rounded-lg p-6 max-w-sm mx-auto border border-gray-200 hover:-translate-y-2 transition-transform"
                 >
-                    <h2 class="text-lg font-bold text-gray-800"><?php echo $user['id']; ?></h2>
-                    <p class="text-lg font-bold text-gray-800 mb-2"><?php echo $user['name']; ?></p>
-                    <div>
+                    <h2 class="text-lg font-bold text-gray-800 text-center"><?php echo $user['title']; ?></h2>
+                    <p class="text-base font-bold text-gray-800"><?php echo $user['id']; ?></p>
+                    <p class="text-base text-gray-600"><?php echo $user['name']; ?></p>
+                    <!-- <div>
                         <label class="font-semibold">หน้าที่ : </label>
                         <p class="text-gray-600"><?php echo $user['detail']; ?></p>
-                    </div>
+                    </div> -->
                 </a>
             </div>
         <?php endforeach; ?>
